b8e106b3d7d148e356addbe4d1399b9ea17e466c
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-08-25 19:09:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# is-hell
- Installing this package installs a bunch of useless packages.
+# is-zone
+
+> Installing this package installs [a bunch of useless packages](https://www.npmjs.com/package/is-zone?activeTab=dependencies).
---
+++ index.js
@@ -0,0 +1,4 @@
+function isZone() {
+    throw Error("You may not run is-zone directly, installing is-zone installs a bunch of other useful packages. See them here: https://www.npmjs.com/package/is-zone?activeTab=dependencies");
+}
+module.exports = isZone;
